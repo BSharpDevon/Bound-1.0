57339c7bab847ebf065c6de195456582b7db9faf
Beth Sharp <beth_sharp@hotmail.co.uk>
2024-11-21 18:00:33 +0000

    set up homepage compenents

---
+++ groupOneBookApp/src/components/homepage.jsx
@@ -0,0 +1,13 @@
+// src/components/HomePage.jsx
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Bound</h1>
+      <p>Find books to read and share with friends.</p>
+    </div>
+  );
+};
+
+export default HomePage;
