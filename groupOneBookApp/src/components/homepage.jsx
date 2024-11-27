2b6884ca7f18e1e0f0175a15ca30d17b9e21d3f7
Beth Sharp <beth_sharp@hotmail.co.uk>
2024-11-27 12:07:28 +0000

    Added username and pw box on homepage

--- groupOneBookApp/src/components/homepage.jsx
+++ groupOneBookApp/src/components/homepage.jsx
@@ -1,13 +1,44 @@
 // src/components/HomePage.jsx
-import React from 'react';
+import React, {useState} from 'react';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to Bound</h1>
-      <p>Find books to read and share with friends.</p>
-    </div>
-  );
+function HomePage(){
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  // This Function is connected to the username box - needs to be written
+  function loginUser(event){
+      setUsername(event.target.value); 
+  }
+
+  function loginPassword(event){
+    setPassword(event.target.value); 
+}
+
+  return (<div>
+            <h1>Welcome to Bound</h1>
+
+            <p>Find books to read and share with friends.</p>
+
+            <p>Username:</p>
+            <input 
+              type="text" 
+              value={username} 
+              placeholder={"Enter your username"}
+              onChange={loginUser}/>
+
+            <p>Password:</p>
+            <input 
+              type="password" 
+              value={password} 
+              placeholder={"Enter your password"}
+              onChange={loginPassword}
+              />
+
+          </div>);
+
+          // PUT IN THE BUTTONS
 };
 
 export default HomePage;
