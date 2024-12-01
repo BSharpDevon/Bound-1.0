bfb13ae713cb769dd5252707d54a24cab15f90ee
[Jenni-Rose] <[jenniscott5@hotmail.com]>
2024-12-01 12:38:17 +0000

    correct errors in favouriteBooks and signUp pages

--- groupOneBookApp/frontend/src/components/signUp.jsx
+++ groupOneBookApp/frontend/src/components/signUp.jsx
@@ -2,98 +2,117 @@
 import React, {useState} from 'react';
 
 // Setting the values to blank
-function signUpPage(){
+function SignUpPage(){
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isChecked, setIsChecked] = useState(false);
 
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
+// These Functions set the new values to state variables
+function loginName(event){
+    setFullName(event.target.value); 
+}
 
-  // These Functions set the new values to each of the above const
-  function loginName(event){
-      setFullName(event.target.value); 
-  }
+// Email validation function
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email); // Basic regex validation for email format
+}
 
-  function loginEmail(event){
-    setEmail(event.target.value); 
-   }
+function loginEmail(event) {
+    const inputEmail = event.target.value;
+    setEmail(inputEmail);
 
-  function loginPassword(event){
-   setPassword(event.target.value); 
-  }
+    // statement to validate if email is correct format 
+    if (!validateEmail(inputEmail)) {
+    console.log("Invalid email format");
+    }
+}
 
-  function privacyCheckbox(event){
+function loginPassword(event){
+setPassword(event.target.value); 
+}
+
+function privacyCheckbox(event){
     setIsChecked(event.target.checked); 
-   }
+}
+
+function signUp() {
+    if (!fullName || !email || !password || !isChecked) {
+    alert("Please fill in all fields and accept the privacy policy to continue");
+    return;
+    }
+    // Code to be edited to submit the data to the API
+    console.log({ fullName, email, password });
+}
 
   // Basic HTML, creating buttons etc
-  return (<div>
+    return (<div>
             {/* Lydia / Emma - Main logo image to be inserted here */}
-            <img src="LOGO IMG NAME" alt="This is the Bound logo" width="500" height="600">
+            <img src="LOGO IMG NAME" alt="This is the Bound logo" width="500" height="600" />
 
-            <p>- YOUR NEXT BEST CHAPTER -</p>
+            <p> - YOUR NEXT BEST CHAPTER - </p>
 
             <p> Sign Up </p>
 
             <label>
-              Full name: 
+            Full name: 
                 <input
-                  type="text" 
-                  value={fullName} 
-                  placeholder={"Enter your full name"}
-                  onChange={loginName}
+                type="text" 
+                value={fullName} 
+                placeholder={"Enter your full name"}
+                onChange={loginName}
                 />
             </label>
 
             <br />
             
             <label>
-              Email:
+            Email:
                 <input 
-                  type="text" 
-                  value={email} 
-                  placeholder={"Enter your email"}
-                  onChange={loginEmail}
+                type="text" 
+                value={email} 
+                placeholder={"Enter your email"}
+                onChange={loginEmail}
                 />
             </label>
 
             <br />
             
             <label>
-              Password:
+            Password:
                 <input 
-                  type="password" 
-                  value={password} 
-                  placeholder={"Enter your password"}
-                  onChange={loginPassword}
+                type="password" 
+                value={password} 
+                placeholder={"Enter your password"}
+                onChange={loginPassword}
                 />
             </label>
 
             <br />
 
             <label>
-              <input
+            <input
                 name="PrivacyCheckbox"
                 type="checkbox"
                 onChange={privacyCheckbox}
 
-              />
+            />
             </label>
 
             <label>
-              <input
+            <input
                 name="signUp"
                 type="button"
                 value="Sign up"
                 onClick={signUp}
-              />
-              By continuing you accept our privacy policy
+            />
+            By continuing you accept our privacy policy
             </label>
 
             {/* Jenni / Jeveria - add URL once created */}
             <p>Already have an account? <a href="url">Sign In</a></p>
 
-          </div>);
+        </div>);
 }
 
-export default signUpPage;
+export default SignUpPage;
