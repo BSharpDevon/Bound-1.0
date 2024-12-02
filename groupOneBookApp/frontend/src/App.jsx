6f4095fff955a1a61ddce33d5283479d32f6a85d
[Jenni-Rose] <[jenniscott5@hotmail.com]>
2024-12-02 12:45:37 +0000

    Changed app.jsx to ensure sign up page is displayed first

--- groupOneBookApp/frontend/src/App.jsx
+++ groupOneBookApp/frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import SignIn from './components/signIn';   // Import the signIn component
+import {useState} from 'react';
 import SignUpPage from './components/signUp';   // Import the SignUp component
+import SignIn from './components/signIn';   // Import the signIn component
 import './App.css';                             // Import the CSS file
 
 function App() {    // Define the App component as a functional component
+  const [isSignUp, setIsSignUp] = useState(true); // State to toggle between SignUp and SignIn
+
   return (          // Returns what should be rendered
-   <div className="App">  
-     <SignIn />
-     <SignUpPage />
-   </div>
- );
+    <div className="App">
+      {isSignUp ? (
+        <SignUpPage setIsSignUp={setIsSignUp} /> 
+      ) : (
+        <SignIn setIsSignUp={setIsSignUp} />
+      )}
+    </div>
+  );
 };
 
 export default App;     // Export the App component so it can be used in other files
