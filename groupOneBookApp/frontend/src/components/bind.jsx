9a61b7d694f40933df3507da1ac91b1fedebb1a1
Jeveria Raja <jeveriaraja@Jeverias-MacBook-Pro.local>
2024-12-04 10:42:14 +0000

    updated bind page

--- groupOneBookApp/frontend/src/components/bind.jsx
+++ groupOneBookApp/frontend/src/components/bind.jsx
@@ -1,9 +1,17 @@
 // src/components/bind.jsx
 
+import { useState } from "react";
+
+//need to create a function/callback which invites a friend to bind - 'inviteFriend'
+
 function BindPage() {
   return (
     <div>
       <h1>Welcome to the Bind Page</h1>
+      <h2>READY TO BIND?</h2>
+      <p>Choose a friend or group to create a bind with - get a personalised book recommendation based on your shared literary tastes</p>
+     
+    <button onClick={inviteFriend }>Invite</button>   
     </div>
   );
 }
